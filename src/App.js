88f2fd5be58f6b389c98d294c975f5aa9db3cd72
Lorsuz <91427731+Lorsuz@users.noreply.github.com>
2023-09-20 07:35:32 -0300

    container

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
 import NavBar from './components/NavBar';
+import Container from './components/Container';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar></NavBar>
+				<Container>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/company" element={<Company />} />
           <Route path="/new-project" element={<NewProject />} />
         </Routes>
+				</Container>
       </Router>
     </div>
   );
